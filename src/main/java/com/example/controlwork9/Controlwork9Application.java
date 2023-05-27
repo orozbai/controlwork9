34bdcf4ab271d1dd887cc20471ef7ae34c1bf10d
Orozbai Altynbekov <altynbekov.bai@gmail.com>
2023-05-27 13:37:22 +0600

    git init

---
+++ src/main/java/com/example/controlwork9/Controlwork9Application.java
@@ -0,0 +1,13 @@
+package com.example.controlwork9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Controlwork9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Controlwork9Application.class, args);
+    }
+
+}
